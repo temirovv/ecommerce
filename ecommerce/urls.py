8f900245b20191f342581a4f5d25f91f43a54953
temirovv <temirovv21@gmail.com>
2025-03-24 09:52:38 +0500

    urls changed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('accounts/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
